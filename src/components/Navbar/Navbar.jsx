0789fcfac74776d7fadf3420419d189dbe241df3
Aleksander <michnowicz@outlook.com>
2024-03-21 17:02:27 +0100

    navbar burger button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,8 @@ import { useNavigate } from "react-router-dom"
 import "./Navbar.css"
 import { useSelector, useDispatch } from "react-redux"
 import { handleLocation, handleDisconnect } from "../../app/features/loginSlice"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import logo from "../../assets/logo.png"
 
 export default function Navbar() {
     
@@ -10,23 +11,22 @@ export default function Navbar() {
     const dispatch = useDispatch()
     const flag = useSelector((state) => state.login.connection.flag)
 
-    // useEffect(()=>{
-    //     console.log(location);
-    // },[location])
+    const [shown, setShown] = useState(false)
 
 
     const handleNav = (e) => {
         navigate(`/${e.target.id}`)
+        setShown(!shown)
     }
 
     return(
         <div className="Navbar">
-            {/* <div className="logo">
-                logo
-            </div> */}
+            <div className="logo">
+                <img src={logo} alt="" />
+            </div>
 
             <div className="linkDiv">
-                <div className="links">
+                {/* <div className="links">
                     <div id='products' onClick={handleNav}>PRODUCTS</div>
                     <div id='guitar' onClick={handleNav}>GUITAR</div>
                     <div id='string' onClick={handleNav}>STRING</div>
@@ -43,17 +43,30 @@ export default function Navbar() {
                     <p>{flag ? "Loged" : ""}</p>
                     <div id='login' onClick={handleNav}>LOG IN</div>
                     {flag ? <div onClick={()=>dispatch(handleDisconnect())}>LOG OUT</div> : ""}
-                </div>
+                </div> */}
                 
             </div>
 
-            {/* <div className="burgerButton">
-                <i className="fa-solid fa-bars"></i>
-                <div className="links">
-                    <button>PRODUCTS</button>
-                    <button>PRODUCTS</button>
+            <div className="burgerButton">
+                <i className="fa-solid fa-bars" onClick={()=>{setShown(!shown)}}></i>
+                <div className={shown ? "linksBurger" : "hidden"} >
+                    <div id='products' onClick={handleNav}>PRODUCTS</div>
+                    <div id='guitar' onClick={handleNav}>GUITAR</div>
+                    <div id='string' onClick={handleNav}>STRING</div>
+                    <div id='pedal' onClick={handleNav}>PEDAL</div>
+                    <div id='amp' onClick={handleNav}>AMP</div>
+                    <div id='tuner' onClick={handleNav}>TUNER</div>
+                    <div id="log">
+                        <div className="NavBasket">
+                            <i className="fa-solid fa-cart-shopping" id="cart" onClick={handleNav}></i>
+                        </div>
+
+                        <p>{flag ? "Loged" : ""}</p>
+                        <div id='login' onClick={handleNav}>LOG IN</div>
+                        {flag ? <div onClick={()=>dispatch(handleDisconnect())}>LOG OUT</div> : ""}
+                    </div>
                 </div>
-            </div> */}
+            </div>
 
         </div>
     )
